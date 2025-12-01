6698c9a00623fe0deec3de8776dbe71e76ac99f3
wchesedh <wchesedh@gmail.com>
2025-12-01 18:34:35 +0800

    update

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,12 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:3000'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
+   'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_origins' => [
+    'http://localhost:3000',
+    'https://react-frontend-pearl-iota.vercel.app'
+],
+'allowed_origins_patterns' => [],
+'supports_credentials' => true,
 
 ];
